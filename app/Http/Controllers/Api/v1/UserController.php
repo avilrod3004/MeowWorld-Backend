6d966f8823b17d8f5016c901c059909c9a9f477c
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-04 18:51:22 +0100

    controlador para la gestion de los usuarios

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller {
+
+    /**
+     * Lista todos los usuarios registrados
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse {
+        $users = User::all();
+        return response()->json([
+            'status' => true,
+            'data' => $users
+        ], 200);
+    }
+
+    /**
+     * Devuelve los datos de un usuario identificado por su id
+     *
+     * @param $id
+     * @return JsonResponse
+     */
+    public function show($id): JsonResponse {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'errors' => 'Usuario no encontrado'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $user
+        ], 200);
+    }
+
+    /**
+     * Devuelve los datos del usuario logueado
+     *
+     * @return JsonResponse
+     */
+    public function profile(): JsonResponse {
+        $user = Auth::user();
+
+        return response()->json([
+            'status' => true,
+            'data' => $user
+        ], 200);
+    }
+
+
+    /**
+     * Actualiza los datos de un usuario identificado por su id
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, $id): JsonResponse {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'errors' => 'Usuario no encontrado'
+            ], 404);
+        }
+
+        $user->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Usuario actualizado correctamente',
+            'data' => $user
+        ], 200);
+    }
+
+    /**
+     * Elimina un usuario identificado por su id
+     * @param $id
+     * @return JsonResponse
+     */
+    public function destroy($id): JsonResponse {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'errors' => 'Usuario no encontrado'
+            ], 404);
+        }
+
+        $user->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Usuario eliminado correctamente',
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,7 +26,14 @@ Route::post('auth/login', [AuthController::class, 'login']);
 
 // rutas protegidas, accesibles por token
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('auth/me', [AuthController::class, 'me']);
+    // Controlador de autenticacion
     Route::get('auth/logout', [AuthController::class, 'logout']);
     Route::post('auth/refresh', [AuthController::class, 'refresh']);
+
+    // Controlador de usuarios
+    Route::get('users', [UserController::class, 'index']);
+    Route::get('users/profile', [UserController::class, 'profile']);
+    Route::get('users/{id}', [UserController::class, 'show']);
+    Route::put('users/{id}', [UserController::class, 'update']);
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
 });
